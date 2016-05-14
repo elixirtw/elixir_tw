60cf67896ca596b08b35a0f88dcddd72cd39a704
Mickey <poverwhelming@gmail.com>
2016-05-14 20:35:04 +0800

    started the template for Post Index

--- web/controllers/post_controller.ex
+++ web/controllers/post_controller.ex
@@ -1,10 +1,8 @@
-defmodule ElixirTw.PageController do
+defmodule ElixirTw.PostController do
   use ElixirTw.Web, :controller
 
   def index(conn, _params) do
     conn
-    |> put_flash( :notice, "ohei" )
-    |> put_flash( :error, "foo" )
     |> render("index.html")
   end
 end
