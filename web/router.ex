049110f93ae4fed646999b0346d100ea723a1ad5
Mickey <poverwhelming@gmail.com>
2016-06-13 12:08:31 +0800

    generated user model

--- web/router.ex
+++ web/router.ex
@@ -5,6 +5,8 @@ defmodule ElixirTw.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
+    plug Guardian.Plug.VerifySession
+    plug Guardian.Plug.LoadResource
     plug :protect_from_forgery
     plug :put_secure_browser_headers
   end
