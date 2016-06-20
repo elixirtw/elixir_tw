130e95bdebcb662f78157a0a23221269a75bf1d5
Mickey <poverwhelming@gmail.com>
2016-06-20 12:44:55 +0800

    added user model and migrations

---
+++ priv/repo/migrations/20160620042914_create_user.exs
@@ -0,0 +1,15 @@
+defmodule ElixirTw.Repo.Migrations.CreateUser do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :name, :string
+      add :email, :string
+      add :crypted_password, :string
+
+      timestamps
+    end
+
+    create unique_index(:users, [:email])
+  end
+end
