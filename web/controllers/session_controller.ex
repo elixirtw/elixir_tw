60304b1cf15f5ae6f89de9e7db2dcee5c05ce4db
Mickey <poverwhelming@gmail.com>
2016-06-20 15:57:03 +0800

    added session_controller

---
+++ web/controllers/session_controller.ex
@@ -0,0 +1,21 @@
+defmodule ElixirTw.SessionController do
+  use ElixirTw.Web, :controller
+
+  alias ElixirTw.User
+  alias ElixirTw.UserQuery
+
+  plug :scrub_params, "user" when action in [:create]
+
+  def create(conn, params = %{}) do
+    #conn
+    #|> put_flash(:info, "Logged in.")
+    #|> Guardian.Plug.sign_in(verified_user)
+    #|> redirect(to: user_path(conn, :index))
+  end
+
+  def delete(conn, _params) do
+    #Guardian.Plug.sign_out(conn)
+    #|> put_flash(:info, "Logged out successfully")
+    #|> redirect(to: "/")
+  end
+end
