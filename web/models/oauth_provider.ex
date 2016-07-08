bea99e24268638abb0204561776bec1253d6bd21
Mickey <poverwhelming@gmail.com>
2016-07-08 20:24:36 +0800

    added OauthProvider schema for user with multiple providers

---
+++ web/models/oauth_provider.ex
@@ -0,0 +1,20 @@
+defmodule ElixirTw.OauthProvider do
+  use ElixirTw.Web, :model
+
+  schema "oauth_providers" do
+    field :provider_name, :string
+    field :provider_id, :string
+    belongs_to :user, ElixirTw.User
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:provider_name, :provider_id])
+    |> validate_required([:provider_name, :provider_id])
+  end
+end
