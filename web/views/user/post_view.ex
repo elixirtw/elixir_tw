0191526b3fb9f67bcb63f15b3a60e3c94a75c812
Mickey <poverwhelming@gmail.com>
2016-09-04 00:09:11 +0800

    finished the layout for new post

--- web/views/user/post_view.ex
+++ web/views/user/post_view.ex
@@ -1,3 +1,12 @@
 defmodule ElixirTw.User.PostView do
   use ElixirTw.Web, :view
+
+  # Inline Script
+  def render("scripts.html", _assigns) do
+    ~E"""
+      <script type="text/javascript" charset="utf-8">
+        $('.ui.dropdown').dropdown()
+      </script>
+    """
+  end
 end
