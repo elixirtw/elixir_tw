c0797a741ee6677a8033de67d4dac3fb0e15c68b
Mickey <poverwhelming@gmail.com>
2016-09-03 22:17:28 +0800

    updated phoenix_ecto

--- web/controllers/user/post_controller.ex
+++ web/controllers/user/post_controller.ex
@@ -2,11 +2,17 @@ defmodule ElixirTw.User.PostController do
   use ElixirTw.Web, :controller
   use Guardian.Phoenix.Controller
 
+  alias ElixirTw.Post
+
   def new(conn, _params, user, _claim) do
     if user do
-      conn |> render("new.html")
+      changeset = Post.changeset(%{})
+      conn |> render("new.html", changeset: changeset)
     else
       conn |> redirect(to: "/login?origin_url=\/posts\/new")
     end
+  end
+
+  def create(conn, params, user, _claim) do
   end
 end
