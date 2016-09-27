329d7544176d17dc086408442dfa32d64402a199
Mickey <poverwhelming@gmail.com>
2016-09-27 22:13:30 +0800

    added slug and excerpt generating

--- web/models/post.ex
+++ web/models/post.ex
@@ -17,7 +17,21 @@ defmodule ElixirTw.Post do
   def changeset(struct, params \\ %{}) do
     struct
     |> cast(params, [:title, :slug, :body, :excerpt])
-    |> validate_required([:title, :slug, :body, :excerpt])
+    |> build_slug
+    |> build_excerpt
+    |> validate_required([:slug])
     |> unique_constraint(:slug)
   end
+
+  defp build_slug(%{title: title, slug: nil}=struct), do: struct |> Map.update!(:slug, "#{slugify_time}-#{slugify_title(title)}")
+  defp build_slug(struct), do: struct
+
+  defp slugify_time, do: DateTime.utc_now |> DateTime.to_unix |> to_string
+  defp slugify_title(title), do: title |> Phoenix.Naming.humanize |> String.replace(" ", "-")
+  defp slugify_title(_), do: ""
+
+  defp build_excerpt(%{body: body, excerpt: nil}=struct), do: struct |> Map.update!(:excerpt, excerptify_body(body))
+  defp build_excerpt(struct), do: struct
+
+  defp excerptify_body(body), do: body |> String.slice(0, 150)
 end
