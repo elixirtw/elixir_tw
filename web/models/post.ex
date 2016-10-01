d6c55ad2b26e4e278dd8aa6afcec0796a12461b2
Mickey <poverwhelming@gmail.com>
2016-10-01 23:49:55 +0800

    make user_id not nullable in database, generate slug properly

--- web/models/post.ex
+++ web/models/post.ex
@@ -14,22 +14,21 @@ defmodule ElixirTw.Post do
   Builds a changeset based on the `struct` and `params`.
   """
   def changeset(struct, params \\ %{}) do
-    params = build_slug(params)
     struct
     |> cast(params, [:title, :slug, :body, :user_id])
-    |> validate_required(:slug)
+    |> build_slug
+    |> validate_required([:slug, :user_id])
     |> unique_constraint(:slug)
     |> assoc_constraint(:user)
   end
 
-  defp build_slug(%{slug: slug} = struct) when not is_nil(slug) do
-    struct
+  defp build_slug(changeset) do
+    build_slug(get_field(changeset, :slug), get_field(changeset, :title))
+    |> (&put_change(changeset, :slug, &1)).()
   end
 
-  defp build_slug(struct) do
-    title = Map.get(struct, :title, "")
-    Map.put(struct, :slug, "#{slugify_time}-#{slugify_title(title)}")
-  end
+  defp build_slug(nil, title), do: "#{slugify_time}-#{slugify_title(title)}"
+  defp build_slug(slug, _), do: slug
 
   defp slugify_time, do: DateTime.utc_now |> DateTime.to_unix |> to_string
 
