e7453b3ab932605f4db3435a9493a96fa0d9f1a2
Daryl Chen <otaq.hsc@gmail.com>
2017-02-10 21:06:58 +0800

    Fix Ecto multiple results error
    ```
    Ecto.MultipleResultsError at GET /auth/facebook/callback
    expected at most one result but got 2 in query:
    from u in ElixirTw.User,
      join: o in ElixirTw.OauthProvider,
      on: true,
      where: o.uid == ^"123456789012345" and o.provider == ^"facebook"
    ```
    It won't raise error if there is only one user in the table.
    To reproduce this issue, login with another user.

--- lib/queries/oauth_query.ex
+++ lib/queries/oauth_query.ex
@@ -1,4 +1,6 @@
 defmodule ElixirTw.OauthQuery do
+  @moduledoc false
+
   use ElixirTw.Web, :query
 
   alias ElixirTw.User
@@ -7,6 +9,7 @@ defmodule ElixirTw.OauthQuery do
   def identify_user(auth) do
     query = from u in User,
             join: o in OauthProvider,
+            on: u.id == o.user_id,
             where: o.uid == ^auth.uid and o.provider == ^Atom.to_string(auth.provider)
 
     { Repo.one(query), auth }
