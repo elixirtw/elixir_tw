8d131b45de788b8b25c7fcddf479e4e11f80fb13
Mickey <poverwhelming@gmail.com>
2017-04-25 20:41:02 +0800

    updated to 1.3-rc

--- mix.exs
+++ mix.exs
@@ -50,7 +50,7 @@ defmodule ElixirTw.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [{:phoenix, "~> 1.3.0-rc", override: true},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
