6386db2e132b74aa3673ffbba13e6b61e5d8c206
Mickey <poverwhelming@gmail.com>
2018-07-22 16:01:44 +0800

    replace brunch with webpack

--- config/dev.exs
+++ config/dev.exs
@@ -11,8 +11,14 @@ config :elixir_tw, ElixirTwWeb.Endpoint,
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
-  watchers: [node: ["node_modules/brunch/bin/brunch", "watch", "--stdin",
-             cd: Path.expand("../assets", __DIR__)]]
+  watchers: [
+    node: [
+      "node_modules/.bin/webpack",
+      "--watch",
+      "--colors",
+      cd: Path.expand("../assets", __DIR__)
+    ]
+  ]
 
 # Watch static and templates for browser reloading.
 config :elixir_tw, ElixirTwWeb.Endpoint,
