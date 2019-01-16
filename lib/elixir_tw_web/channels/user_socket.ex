88e0a83e5a37ef413c8bd0db879ae3635ebd624b
Orga Shih <orga.shih@gmail.com>
2019-01-16 10:28:18 +0800

    minor change: correct module name in comment

--- lib/elixir_tw_web/channels/user_socket.ex
+++ lib/elixir_tw_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ElixirTwWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", SampleWeb.RoomChannel
+  # channel "room:*", ElixirTwWeb.RoomChannel
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
@@ -26,7 +26,7 @@ defmodule ElixirTwWeb.UserSocket do
   # Would allow you to broadcast a "disconnect" event and terminate
   # all active sockets and channels for a given user:
   #
-  #     SampleWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
+  #     ElixirTwWeb.Endpoint.broadcast("user_socket:#{user.id}", "disconnect", %{})
   #
   # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
